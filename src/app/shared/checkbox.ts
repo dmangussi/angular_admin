a33ec05d78cc54a60cb016f101905751d130b109
dmangussi <dmangussi@gmail.com>
2017-06-29 17:47:04 -0300

    Added dynamic checkbox.

---
+++ src/app/shared/checkbox.ts
@@ -0,0 +1,5 @@
+export class Checkbox
+{
+  description: string;
+  value: string;
+}
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,5 +1,6 @@
+import { Checkbox } from './../shared/checkbox';
 import { Http } from '@angular/http';
-import { FormGroup, FormBuilder, Validators, FormControl, ValidationErrors } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl, ValidationErrors, FormArray } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import 'rxjs/add/operator/map';
 
@@ -14,11 +15,10 @@ export class UsersComponent implements OnInit {
   successMessage: boolean;
   errorMessage: boolean;
   errorResponse: Response;
-
-  rules: Array<any> = [
+  rules: Array<Checkbox> = [
     {description: 'Admin', value: 'ROLE_ADMIN'},
     {description: "Normal", value: 'ROLE_NORMAL'},
-    {description: "Guess", value: 'ROLE_GUESS'}
+    {description: "Guess", value: 'ROLE_GUESS'},
   ];
 
   constructor
@@ -29,15 +29,27 @@ export class UsersComponent implements OnInit {
 
   ngOnInit()
   {
-    this.userForm = this.formBuilder.group({
+    let arrayRules: FormArray = new FormArray([]);
+    for (let i = 0; i < this.rules.length; i++)
+    {
+      let formGroup = new FormGroup({});
+      formGroup.addControl(this.rules[i].value, new FormControl(false));
+      arrayRules.push(formGroup);
+    }
+
+    this.userForm = this.formBuilder.group
+    ({
       name: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(20)]],
       password: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(10)]],
-      confirmPassword: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(10)]]
+      confirmPassword: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(10)]],
+      rules: arrayRules
     });
   }
 
   onSubmit()
   {
+    console.log(JSON.stringify(this.userForm.value))
+
     this.successMessage = false;
     this.errorMessage = false;
 
@@ -72,28 +84,30 @@ export class UsersComponent implements OnInit {
 
   applyCSSError(field : string)
   {
-    if (this.verifyValidTouched(field)) {
+    if (this.verifyValidTouched(field))
+    {
       return 'has-danger'
     }
   }
 
-  getFieldError(field) : ValidationErrors
+  getFieldError(field: string) : Array<string>
   {
     let errors: Array<string> = new Array<string>();
+    let vErrors: ValidationErrors = this.userForm.get(field).errors;
 
-    if (this.userForm.get(field).errors)
+    if (vErrors)
     {
-      if (this.userForm.get(field).errors['required'])
+      if (vErrors['required'])
       {
         errors.push("This field is required");
       }
-      if (this.userForm.get(field).errors['minlength'])
+      if (vErrors['minlength'])
       {
-        errors.push("Minimum length: " + this.userForm.get(field).errors['minlength'].requiredLength);
+        errors.push("Minimum length: " + vErrors['minlength'].requiredLength);
       }
-      if (this.userForm.get(field).errors['maxlength'])
+      if (vErrors['maxlength'])
       {
-        errors.push("Maximum length: " + this.userForm.get(field).errors['maxlength'].requiredLength);
+        errors.push("Maximum length: " + vErrors['maxlength'].requiredLength);
       }
 
       return errors;
